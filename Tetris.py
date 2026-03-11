ea7e245db9f22db131ab3628981c897a0ccaf84c
G-ENC <bartugenc@gmail.com>
2026-03-11 20:14:21 +0300

    .

--- Tetris.py
+++ Tetris.py
@@ -1,5 +1,16 @@
 import pygame
 import numpy 
+import math
+
+def checker_pattern():
+  for y in range(w//4):
+    for x in range(h//4):
+      test_surf = pygame.Surface((15,15))
+      if((x)+(y))%2 == 1:
+        test_surf.fill("red")
+      else:
+        test_surf.fill((0, 0, 0))
+      screen.blit(test_surf,(15*x,y*15))
 
 def drawgrid():
   for row in range(14):
@@ -9,7 +20,7 @@ def drawgrid():
 
 #draws screen based on array
 def draw_10x14_tetris_array(tetris_array):
-  for y in range(13,0,-1):
+  for y in range(13,-1,-1):
     for x in range(10):
       if(tetris_array[y][x]==1):
         # print(f'x: {w/10*x} y: {h/10*y}')
@@ -17,44 +28,33 @@ def draw_10x14_tetris_array(tetris_array):
       elif tetris_array[y][x] == 2:
         pygame.draw.rect(screen,"dark orange",pygame.Rect(w/10*x, h/14*y, w/10 + 1 ,h/14 + 1))
 
-
-def falling_logic_from_array(array1,array2):
-  for y in range(13,0,-1):
+def falling_logic_from_array(tetris_array):
+  for y in range(13,-1,-1):
     for x in range(10):
-      
-      if(array1[y][x]==1):
-        
-        if y+2 == 14 :
-          array2[y+1][x] = 2
-          for sub_y in range(12,0,-1):
-            if array1[sub_y][x] == 1 or array2[sub_y][x] == 1:
-              print("asfsafasf")
-              array2[sub_y][x] = 2
-              array1[sub_y][x] = 2
-          array1[y][x] = 0
-        elif (array1[y+1][x] == 2 or array2[y+1][x] == 2):
-          print("runing!")
-          array1[y][x] = 0
-          array2[y][x] = 2
-          for sub_y in range(13,0,-1):
-            if array1[sub_y][x] == 1 or array2[sub_y][x] == 1:
-              print("asfsafasf")
-              array2[sub_y][x] = 2
-              array1[sub_y][x] = 2
-        else:
-          # print("muah")
-          array2[y+1][x] = 1
-          array1[y][x] = 0
-      # elif array1[y][x] == 2:
-      #      for sub_y in range(14):
-      #       if(array1[sub_y][x] == 1 or array2[sub_y][x]==1):
-      #         print("ahh")
-      #         array2[sub_y][x] = 2
-  temp_array = array1
-  array1 = array2
-  array2 = temp_array
 
-  draw_10x14_tetris_array(array1)
+      if tetris_array[y][x]==1 :
+        #if touched bottom freeze
+        if y+1 == 14 :
+          tetris_array[y][x] = 2
+
+          #freeze the same column
+          #idea: search if until the adjascent top of the block is 1 unless LEAVE TGHE FUCK OUT 
+          #result: KUDOS
+          for sub_y in range(12,0,-1):
+            if tetris_array[sub_y][x] == 2:
+              if tetris_array[sub_y+1][x] == 1:
+                tetris_array[sub_y][x] = 2
+        
+        #freeze if there is a block beneath that is frozen
+        elif tetris_array[y+1][x] == 2 :
+          tetris_array[y][x] = 2
+        
+        #drop square
+        else:
+          tetris_array[y][x] = 0
+          tetris_array[y+1][x] = 1
+          
+  draw_10x14_tetris_array(tetris_array)
 
 pygame.init()
 screen = pygame.display.set_mode((1600,1200),pygame.RESIZABLE)
@@ -80,18 +80,18 @@ test_tetris_array =  [[0,0,1,0,0,0,0,0,0,0],
                       [1,0,0,0,0,0,0,0,0,0],]
 
 test_tetris_array2 = [[0,0,0,0,1,0,0,0,0,0],
+                      [0,0,1,1,1,1,0,0,0,0],
+                      [0,0,0,0,1,1,1,0,0,0],
+                      [0,0,1,0,1,0,0,0,0,0],
+                      [0,0,0,1,0,0,0,0,0,0],
+                      [0,0,0,1,0,1,1,0,0,0],
+                      [0,0,0,0,1,0,0,0,0,0],
+                      [0,0,0,1,1,0,0,0,0,0],
+                      [0,0,0,0,1,1,1,0,0,0],
+                      [1,1,1,1,1,1,1,1,1,1],
                       [0,0,0,0,1,0,0,0,0,0],
                       [0,0,0,0,1,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0,0],
                       [0,0,0,0,1,0,0,0,0,0],
-                      [0,0,0,0,1,0,0,0,0,0],
-                      [0,0,0,0,1,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0,0],
                       [0,0,0,0,0,0,0,0,0,0],]
 
 new_array =          [[0,0,0,0,0,0,0,0,0,0],
@@ -118,75 +118,56 @@ while True:
   screen_dimension_text_rect = screen_dimension_text_surf.get_rect(center=(w/2,h/2))
   screen.blit(screen_dimension_text_surf,screen_dimension_text_rect)
 
+#event handler
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
       exit()
+    
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      mouse_x_array = math.floor(event.pos[0]/(w/10))
+      mouse_y_array = math.floor(event.pos[1]/(h/14))
+      print(mouse_y_array,mouse_x_array)
+      test_tetris_array2[mouse_y_array][mouse_x_array] = 1
+
+      
 
   
   screen.fill("white")
 
-#draw checker pattern
-  # for y in range(w//4):
-  #   for x in range(h//4):
-  #     test_surf = pygame.Surface((15,15))
-  #     if((x)+(y))%2 == 1:
-  #       test_surf.fill("red")
-  #     else:
-  #       test_surf.fill((0, 0, 0))
-  #     screen.blit(test_surf,(15*x,y*15))
-  
-  # line grid
+# draw checker pattern
+  checker_pattern()
+
+
   drawgrid()
 
-  # take 14x10 array --> colors that specific part of the array which is 1 in grid
-  # 
-
-  
-
-  for y in range(13,-1,-1):
-    for x in range(10):
-
-      if(test_tetris_array2[y][x]==1 ):
-        if y+1 == 14 :
-          test_tetris_array2[y][x] = 2
-        
-          for sub_y in range(12,0,-1):
-            if test_tetris_array2[sub_y][x] == 1:
-              test_tetris_array2[sub_y][x] = 2
-
-        elif test_tetris_array2[y+1][x] == 2 :
-          test_tetris_array2[y][x] = 2
-        
-        else:
-          test_tetris_array2[y+1][x] = 1
-          test_tetris_array2[y][x] = 0
-
-  
-    
-
-  #example syntaxs
-    # to make a square 
-    # pygame.draw.rect(screen,"orange",pygame.Rect(coordinate x,coordinate y,box x,box y))
-
-  # traverse every cell if value equals 1 put a square beneath it and delete the current cell in the original
-  # make the first array equal to the second array
-  # after every time we put all squares in to their new position print the array
-
-  a = numpy.array(test_tetris_array2)
-  b = numpy.array(new_array)
-  
-  print("test")
-  print(a)
-  print("new")
-  print(b)
-  draw_10x14_tetris_array(test_tetris_array2)
-
-  # falling_logic_from_array(test_tetris_array2,new_array)
+  falling_logic_from_array(test_tetris_array2) 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+  pygame.display.update()
+  clock.tick(5)
+
+
+#example syntaxs:-------------------------------------------
+  # to make a square 
+  # pygame.draw.rect(screen,"orange",pygame.Rect(coordinate x,coordinate y,box x,box y))
+
+  #numpy 2d array pretty in print
+  # a = numpy.array(test_tetris_array2)
+  # b = numpy.array(new_array)
 
 
 #idea 1
@@ -201,21 +182,9 @@ while True:
   #how to mkae custom hitboxes are issue for another day
 
 
-
-
-
-  
-
-
-
-
 #notes:
   # maybe drawing the array based on the position isnt suitable since 
   # the game will be jittry and skip[ the interavls of steps it take 
   # gonna retry with a rectangel object that falss based on its y value
   # and checking for collsions taht way will resolve my issue ig
-
-  #scratch this 
-
-  pygame.display.update()
-  clock.tick(5)
+  #scratch this original tetris is jittry too 
